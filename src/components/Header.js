a40f260c19cc0627f89671b695dbc140cf2ac0e3
rajeshwari2538 <rajeshwari2538@gmail.com>
2024-01-24 20:52:21 +0530

    browse page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { addUser, removeUser } from '../utils/userSlice';
+import { LOGO, USER_AVATAR } from '../utils/constants';
 
 const Header = () => {
 
@@ -37,9 +38,9 @@ const Header = () => {
 
   return (
     <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
-        <img className='w-44 ' src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo"/>
+        <img className='w-44 ' src={LOGO} alt="logo"/>
         <div className='flex p-2'>
-          <img className='w-12 h-12' src="https://occ-0-3647-3646.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229" alt="user-img"/>
+          <img className='w-12 h-12' src={USER_AVATAR} alt="user-img"/>
           <button className='font-bold text-white' onClick={handleSignOut}>Sign Out</button>
         </div>
     </div>
---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const USER_AVATAR = "https://occ-0-3647-3646.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
+
+export const API_OPTIONS= {
+    method: 'GET',
+    hostname: 'api.themoviedb.org',
+    port: null,
+    path: '/3/movie/now_playing?language=en-US&page=1',
+    headers: {
+      accept: 'application/json',
+      Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDJkMmY2ODRkZmZjYTYzMjViNjQ5NTcwYjQzNjA4MiIsInN1YiI6IjY1YjBkZjkxZWEzN2UwMDE3MmU0NGI2MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.INZZJdtHb56peA_ukDt4NmeG12XcpwL9TmWLFZTFWu8"
+    }
+  };
